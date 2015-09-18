5fb6faa1fe9b4bd807d47f3cda735d6b94222f89
yeliex <yeliex@yeliex.com>
2015-09-18 15:40:25 +0800

    上传1.2版本的文件.包含所有需要的东西

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # yeliex.Carousel
 一个简单的Carousel Jquery插件
+
+## 插件信息
+### 作者: yeliex (yeliex.com)
+### 插件网站: work.yeliex.com/carousel
+### 版本: 1.2
+### 更新: 2015-09-18
+
+## 预览
+
+## 用法
+
+	引入依赖文件: basic.css,yCarousel.min.js
+	
+	<div class="wrapper"> // 需要一个容器
+		<div class="carousel" data-content="true"> // 插件会检测页面中的每一个carousel,所以这个样式是必须的.data-content=true代表需要文本显示的遮罩层
+        	<div class="list active" data-img="img1" data-content="标题1/内容1"></div> // list样式代表这是一个子项,active代表页面加载的时候从这个项目开始显示.active不强制加在第一个上面,但只能有一个.会从带有active的项目开始循环
+        	<div class="list" data-img="img2" data-link='#' data-content="标题2/内容2"></div> // data-img中的内容是图片的地址
+        	<div class="list" data-img="img3" data-link='#' data-content="标题3/内容3"></div> // data-link中的内容是链接地址,如果不需要点击以后打开新页面则留空/不填
+        	<div class="list" data-img="img4" data-link='#' data-content="标题4/内容4"></div> // 这里的data-content代表要在图片上显示的内容,标题/内容以"/"分割
+    	</div>
+	</div>
+	
+	后面会有计划加入对html的元素支持,目前没有这个需求:)
---
+++ src/yCarousel.js
@@ -0,0 +1,150 @@
+/**
+ *  yCarousel
+ *  一款简单的Carousel Jquery插件
+ *
+ *  Author: yeliex
+ *          yeliex.com
+ *  website: work.yeliex.com/carousel
+ *
+ *  version :1.2
+ *  date: 2015-09-18
+ */
+
+$(function () {
+    // 首先获取页面上的所有.carousel对象
+    var object = $(".carousel");
+    var objectNum = object.length;
+    // 循环对每个carousel对象进行处理
+    for (var i = 0; i < objectNum; i++) {
+        doList(object[i]);
+    }
+
+    function doList(target) {
+        // 创建一个公共变量来处理
+        var obj = new InitList(target);
+        // 首先设置默认要显示的图片
+        initSet(obj);
+        obj.current = 1;
+
+        // 开始循环处理
+        setInterval(function () {
+            // 获取下一个的序号
+            var next = Number(obj.current) + 1;
+            if (next == (obj.list.length+1)) next = 1;
+            // 设置图片
+            var item = obj.list[next - 1];
+            console.log(item);
+            setBG(item.img, item.link, obj.target);
+            if (obj.content.content == "true") {
+                var content = obj.content.list[next - 1];
+                setContent(obj.target, content.a, content.b);
+            }
+            else {
+                $(target).html(" ");
+            }
+            obj.current = next;
+        }, 4000);
+    }
+
+    function InitList(target) {
+        var itemList = $(target).children('.list');
+        this.target = $(target);
+        this.active = getActive(itemList);
+        this.list = getList(itemList);
+        this.current = 0;
+        this.content = getContent(itemList, target);
+        function getActive(list) {
+            for (var i = 0; i < list.length; i++) {
+                var item = $(list[i]);
+                if (item.hasClass('active')) {
+                    return item;
+                }
+            }
+            // 如果没有item有active样式,则指定为第一个
+            return $(list[0]);
+        }
+
+        function getList(list) {
+            var result = new Array();
+            for (var i = 0; i < list.length; i++) {
+                result[i] = {
+                    img: $(list[i]).attr('data-img'),
+                    link: $(list[i]).attr('data-link')
+                }
+            }
+            return result;
+        }
+
+        function getContent(list, target) {
+            var content = $(target).attr('data-content');
+            if (content) {
+                // 需要内容的时候,循环读取内容
+                var result = new Array();
+                for (var i = 0; i < list.length; i++) {
+                    result[i] = {
+                        a: $(list[i]).attr('data-content').split("/")[0],
+                        b: $(list[i]).attr('data-content').split("/")[1]
+                    }
+                }
+            }
+
+            return {
+                content: content,
+                list: result
+            }
+        }
+    }
+
+    function initSet(obj) {
+        var item = obj.active;
+        var target = obj.target;
+        var link = item.attr('data-link');
+        var img = item.attr('data-img');
+        // 设置属性
+        var styleStr = "background: url('" + img + "') top center no-repeat; background-size: cover;";
+        if (link != null) {
+            styleStr += " cursor: pointer !important;";
+            // 有链接的时候需要设置点击事件
+            target.click(function () {
+                window.open(link);
+            })
+        }
+        target.attr('style', styleStr);
+        if (obj.content.content == "true") {
+            var a = item.attr('data-content').split("/")[0];
+            var b = item.attr('data-content').split("/")[0];
+            setContent(obj.target, a, b);
+        }
+        else {
+            $(target).html(" ");
+        }
+    }
+
+    function setBG(img, link, target) {
+        // 设置属性
+        target.unbind("click");
+        var styleStr = "background: url('" + img + "') top center no-repeat; background-size: cover;";
+        if (link != null) {
+            styleStr += " cursor: pointer !important;";
+            // 有链接的时候需要设置点击事件
+            target.click(function () {
+                window.open(link);
+            })
+        }
+        target.attr('style', styleStr);
+    }
+
+    function setContent(target, a, b) {
+        // 设置内容
+        // 需要先隐藏再显示以避免闪烁,如果图片较大或加载慢需要增加动画hide(),show()时间
+        $(target).children(".content").children().hide();
+        var htmlStr = "<div class='content'>";
+        if (a != null && b != null) {
+            htmlStr += "<div class='a'>" + a + "</div>";
+            htmlStr += "<div class='b'>" + b + "</div>";
+        }
+        htmlStr += "</div>";
+        $(target).html(htmlStr);
+        $(target).children(".content").children().show();
+    }
+});
